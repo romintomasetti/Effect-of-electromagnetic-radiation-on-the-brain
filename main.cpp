4afe29518e252c4dc993abaecb5b4ea814a89103
romintomasetti <romin.tomasetti@gmail.com>
2018-02-14 18:05:40 +0100

    Adding the main file (c++)

---
+++ main.cpp
@@ -0,0 +1,11 @@
+/*
+ * Very basic libraries.
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+
+int main(int argc, char* argv[]){
+	cout << "This is the begining og a very exciting project!\n" <<;
+	return EXIT_SUCCESS;
+}
