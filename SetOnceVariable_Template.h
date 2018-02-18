5f12a12b6fb407c5dba8356ac5c3287341b28e7d
romintomasetti <romin.tomasetti@gmail.com>
2018-02-18 12:09:44 +0100

    Adding a template for variables that can only be set once.

---
+++ SetOnceVariable_Template.h
@@ -0,0 +1,30 @@
+/* This is a template for providing variables that can be set only once */
+/* That is, when it has been set once, it can never be changed.         */
+#ifndef SETONCEVARIABLE_TEMPLATE_H
+#define SETONCEVARIABLE_TEMPLATE_H
+
+template<typename T>
+class SetOnceVariable_Template{
+	private:
+		T value;
+		bool alreadySet = false;
+	public:
+		// Default constructor:
+		SetOnceVariable_Template()       : value(0)    {};
+		// Constructor:
+		SetOnceVariable_Template(T init) : value(init) {};
+		// Destructor:
+		~SetOnceVariable_Template(void){};
+		// Set the value / overloading of the '=' operator:
+		SetOnceVariable_Template<T>& operator=(const T& value){
+			if(this->alreadySet == false){
+				this->value = value;
+				this->alreadySet = true;
+			}
+			return *this;
+		}
+		// Get the value:
+		const T& get(void){return this->value;}
+};
+
+#endif
