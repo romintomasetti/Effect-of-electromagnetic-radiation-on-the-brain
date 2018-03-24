64716623b406e7fa402f9242fc7e4c8d01947cd5
romintomasetti <romin.tomasetti@gmail.com>
2018-03-24 14:25:05 +0100

    Correcting sh file

--- SLURM_JOBS/AlgoElectroScaling.sh
+++ SLURM_JOBS/AlgoElectroScaling.sh
@@ -3,10 +3,10 @@
 #SBATCH --job-name=AlgoElectroSclaing
 #SBATCH --output=AlgoElectroScaling.output
 #
-#SBATCH --ntasks=$BR_TASKS
+#SBATCH --ntasks=1
 #SBATCH --mem-per-cpu=1000
 #SBATCH --time=1:00
 
 export OMP_NUM_THREADS=$OMP_NBR_THRDS
 
-mpirun ./main -inputfile $INPUTFILE
+mpirun ./main -inputfile $INPUTFILE -whichAlgo ELECTRO
