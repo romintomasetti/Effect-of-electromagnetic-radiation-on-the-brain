5235613de5b0b5c904ccca37ef1c9c05479f2918
romintomasetti <romin.tomasetti@gmail.com>
2018-03-25 00:01:27 +0100

    Creating sbatch submission file

--- SLURM_JOBS/AlgoElectroScaling.sh
+++ SLURM_JOBS/AlgoElectroScaling.sh
@@ -3,11 +3,12 @@
 #SBATCH --job-name=AlgoElectroScaling
 #SBATCH --output=AlgoElectroScaling.output
 #
-#SBATCH --mem-per-cpu=5000
-#SBATCH --time=5:00
+#SBATCH --mem-per-cpu=40000
+#SBATCH --time=30:00
 
 export OMP_NUM_THREADS=$OMP_NBR_THRDS
 
 VAR=$((echo "### NBR_MPI = $SLURM_NTASKS AND OMP = $OMP_NUM_THREADS ###"
-time mpirun ./main -inputfile $INPUTFILE -whichAlgo ELECTRO) 2>&1)
+echo "### input is $INPUTFILE###"
+time mpirun ./main -inputfile $INPUTFILE) 2>&1)
 flock -x -w 500 AlgoElectroScaling_outfile echo "$VAR" >> AlgoElectroScaling_outfile
