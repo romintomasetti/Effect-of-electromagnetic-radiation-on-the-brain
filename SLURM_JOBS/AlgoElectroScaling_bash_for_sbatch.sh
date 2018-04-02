af56c985348945576076503c492a6457a0e353a5
romintomasetti <romin.tomasetti@gmail.com>
2018-04-02 19:10:50 +0200

    self-

--- SLURM_JOBS/AlgoElectroScaling_bash_for_sbatch.sh
+++ SLURM_JOBS/AlgoElectroScaling_bash_for_sbatch.sh
@@ -1,30 +1,29 @@
+#
 # This bash file eases the scaling tests.
 # The first loop is on the number of MPI processes.
 # The second loop is on the number of OPENMP threads.
 # The third loop is for statistics.
 #
 # Maximum simultaneous jobs:
-MAX_JOBS=5
-for NBR_MPI in {1..1}
-do
-        for NBR_OMP in {1..1}
-        do
-                for i in {1..1}
-                do
-                        NS=$(squeue | grep rtoma)
-                        NS=$(echo "$NS" | wc -l)
-                        MAX=8
-                        while [ "$NS" -gt "$MAX_JOBS" ]
-                        do
-                                sleep 5
-                                NS=$(squeue | grep rtoma)
-                                NS=$(echo "$NS" | wc -l)
-                                echo "Number of slurm tasks is $NS (max $MAX)"
-                        done
-                        echo "### NP = $NP AND K = $K ###"
-                        sbatch --export=OMP_NBR_THRDS=$NBR_OMP,INPUTFILE=$INPUTFILE \
-                                --ntasks $NBR_MPI \
-                                AlgoElectroScaling.sh
-                done
-        done
-done
+MAXJOBS=5
+# Go for it:
+# for NBRMPI in {1..2}
+# do
+	# for NBROMP in {1..2}
+	# do
+			# for i in {1..2}
+			# do
+					# NS=$(squeue | grep rtoma)
+					# NS=$(echo "$NS" | wc -l)
+					# while [ "$NS" -gt "$MAXJOBS" ]
+					# do
+							# sleep 5
+							# NS=$(squeue | grep rtoma)
+							# NS=$(echo "$NS" | wc -l)
+							# echo "Number of slurm tasks is $NS (max $MAX)"
+					# done
+					# echo "### NP = $NBRMPI AND K = $NBROMP ###"
+					# sbatch --export=OMP_NBR_THRDS=$NBROMP,INPUTFILE="$INPUTFILE" --ntasks $NBRMPI AlgoElectroScaling.sh
+			# done
+	# done
+# done
