c5e49be6b8b6c1f529e3116ecb154f00ed5e181f
romintomasetti <romin.tomasetti@gmail.com>
2018-04-09 11:23:03 +0200

    geometry file for air everywhere

---
+++ UTILS/vector_utilities.hpp
@@ -0,0 +1,8 @@
+void fill_double_vector_with_zeros(double *vec,size_t size){
+    for(size_t I = 0 ; I < size ; I ++)
+        vec[I] = 0.0;
+}
+void fill_unchar_vector_with_zeros(unsigned char *vec,size_t size){
+    for(size_t I = 0 ; I < size ; I ++)
+        vec[I] = 0;
+}
